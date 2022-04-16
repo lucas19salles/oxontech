8132c48d8587e0718500738edb8a1af5dde65596
lucas19salles <lucassalles19@hotmail.com>
2022-04-16 12:14:28 -0300

    animação

--- script.js
+++ script.js
@@ -14,3 +14,45 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
+
+// Animação do site
+
+const debounce = function (func, wait, immediate) {
+  let timeout;
+  return function (...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animate";
+
+function animeScroll() {
+  const windowTop = window.scrollY + (window.innerHeight * 3) / 3;
+  target.forEach(function (element) {
+    if (windowTop > element.offsetTop) {
+      element.classList.add(animationClass);
+    } else {
+      element.classList.remove(animationClass);
+    }
+  });
+}
+
+animeScroll();
+
+if (target.length) {
+  window.addEventListener(
+    "scroll",
+    debounce(function () {
+      animeScroll();
+    }, 200)
+  );
+}
